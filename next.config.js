b6d661e4caf850932b6841649eea67a0740d5611
Samda <muy.samda13@gmail.com>
2022-10-16 11:27:17 +0700

    init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    // providing the locales supported by your application
+    locales: ["en","fr" , "es-ES", "it-IT"],
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: "en",
+    localeDetection: false
+  },
+  optimizeFonts: true
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'footer-image': "url('/images/footer-image.jpg')",
+        'header-image': "url('/images/header-image.jpg')",
+        'busket-image': "url('/images/busket.jpg')",
+        'logo-en': "url('/images/logo-en.png')",
+        'logo-fr': "url('/images/logo-fr.png')",
+      }
+    },
+    letterSpacing: {
+      large: '.50em'
+    },
+    colors: {
+      'primary': '#379482',
+      'secondary': '#D8BC86',
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'dark-blue': "#1C4D7F"
+    }
+  },
+  plugins: [],
+}
