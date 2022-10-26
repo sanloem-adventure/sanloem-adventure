82a7dd8cda307e777c32642ef746909f124eb97b
Samda <muy.samda13@gmail.com>
2022-10-26 14:10:32 +0700

    improve responsive design

--- components/HeaderSession.js
+++ components/HeaderSession.js
@@ -6,7 +6,7 @@ const HeaderSession = ({ header_title, header_description, title, logo }) => {
 
   const router = useRouter()
   return (
-    <div className={`flex flex-col w-full h-auto`}>
+    <div className={`flex flex-col w-full h-auto mb-10 lg:mb-28`}>
       <img
         width="100%"
         height="100%"
@@ -14,13 +14,13 @@ const HeaderSession = ({ header_title, header_description, title, logo }) => {
         className="absolute object-contain md:object-cover"
         alt="header image"
       />
-      <nav className="z-20 md:flex-row md:justify-between absolute flex flex-col w-full">
+      <nav className="z-20 md:flex-row lg:flex-row md:justify-between absolute flex flex-col w-full">
         <Link href="/">
           <a className="md:mt-10 md:indent-10 2xl:text-xl xl:text-lg md:text-normal md:tracking-[.5em] indent-6 mt-4 text-sm font-light tracking-[.2em] text-start text-white">{title}</a>
         </Link>
-        <div className="2xl:pl-60 2xl:gap-48 2xl:pl-60 2xl:relative xl:pl-32 xl:relative xl:gap-32 md:flex-row md:w-1/2 md:py-8 md:text-lg sm:absolute sm:left-0 text-sm self-end py-5 px-5 w-full flex flex-col items-end font-light text-white gap-2">
+        <div className="2xl:pl-60 2xl:gap-48 2xl:pl-60 2xl:relative xl:pl-32 md:relative xl:relative md:gap-28 xl:gap-32 md:flex-row md:w-1/2 md:py-8 md:text-lg sm:absolute sm:left-0 text-sm self-end py-5 px-5 w-full flex flex-col items-end font-light text-white gap-2">
           <Link href="/#lot">
-            <a className="px-2 py-2 cursor-pointe">LOTS</a>
+            <a className="cursor-pointe">LOTS</a>
           </Link>
           <Link href="/#contact">
             <a className="hover:cursor-pointer">CONTACTE</a>
@@ -45,8 +45,8 @@ const HeaderSession = ({ header_title, header_description, title, logo }) => {
             <img className="xl:relative absolute xl:left-auto left-6 xl:top-auto top-16 w-28 h-16 xl:w-64 xl:h-40" src={logo} />
           </div>
           <div className="flex flex-col self-center text-white xl:mt-10 mt-16 xl:gap-8 gap-10">
-            <div className="flex flex-col xl:px-44 xl:visible invisible">
-              <h2 className="2xl:visible xl:visible invisible text-center text-3xl font-normal uppercase leading-loose">{header_title}</h2>
+            <div className="flex flex-col xl:px-44 2xl:visible xl:visible md:visible lg:visible invisible">
+              <h2 className="2xl:visible lg:visible md:visible xl:visible invisible text-center text-3xl font-normal uppercase leading-loose">{header_title}</h2>
             </div>
             <a href="#contact" className="xl:relative absolute xl:top-auto top-28 xl:right-auto xl:self-center xl:right-auto right-4 uppercase xl:text-normal xl:text-xl text-xs text-center text-white font-light bg-dark-blue py-1 px-2 xl:py-4 rounded-lg xl:w-4/12 xl:px-2 xl:mt-2 mt-10">Contact US</a>
           </div>
